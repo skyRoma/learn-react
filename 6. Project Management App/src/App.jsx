09fb33d40317e76f7fc309576d2970cea4a4ae79
Roman Sluka <r.sluka@itransition.com>
2024-01-10 18:35:59 +0400

    Added no-project-selected component.

--- 6. Project Management App/src/App.jsx
+++ 6. Project Management App/src/App.jsx
@@ -1,11 +1,32 @@
+import { useState } from 'react';
 import { NewProject } from './components/NewProject';
+import { NoProjectSelected } from './components/NoProjectSelected';
 import { ProjectsSidebar } from './components/ProjectsSidebar';
 
 export const App = () => {
+  const [projects, setProjects] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  const handleStartAddProject = () => {
+    setProjects((prevProjects) => {
+      return { ...prevProjects, selectedProjectId: null };
+    });
+  };
+
+  let content;
+
+  if (projects.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  } else if (projects.selectedProjectId === null) {
+    content = <NewProject />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NewProject />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 };
---
+++ 6. Project Management App/src/components/NoProjectSelected.jsx
@@ -0,0 +1,18 @@
+import { Button } from './Button';
+
+export const NoProjectSelected = ({ onStartAddProject }) => {
+  return (
+    <div className="mt-24 text-center w-2/3">
+      <img src="./logo.png" className="w-16 h-16 object-contain mx-auto" />
+      <h2 className="text-xl font-bold text-stone-500 my-4">
+        No project selected
+      </h2>
+      <p className="text-stone-400 mb-4">
+        Select a project from the sidebar or create a new one.
+      </p>
+      <p className="mt-8">
+        <Button onClick={onStartAddProject}>Create new project</Button>
+      </p>
+    </div>
+  );
+};
