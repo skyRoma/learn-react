55b446bcfe7daac9e62a06f46db8c7e29e1d31b2
Roman Sluka <r.sluka@itransition.com>
2024-01-18 16:01:08 +0400

    Added 12 app skeleton.

---
+++ 12. Forms & User Input/src/App.jsx
@@ -0,0 +1,13 @@
+import { Header } from './components/Header.jsx';
+import { Login } from './components/Login.jsx';
+
+export const App = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <Login />
+      </main>
+    </>
+  );
+};
---
+++ 12. Forms & User Input/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export const Header = () => {
+  return (
+    <header>
+      <img src="logo.jpg" alt="A form and a pencil" />
+      <h1>React Forms</h1>
+    </header>
+  );
+};
