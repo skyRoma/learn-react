eab4032bc446dacce755fe7b829062d0e513cada
Roman Sluka <r.sluka@itransition.com>
2024-01-27 22:03:02 +0400

    Added 15th app skeleton.

---
+++ 15. Redux/src/components/Counter.js
@@ -0,0 +1,13 @@
+import classes from './Counter.module.css';
+
+export const Counter = () => {
+  const toggleCounterHandler = () => {};
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
