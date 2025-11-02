cfac8f1719efe3456296eb0ec3a3c0672f3ee3b8
Roman Sluka <roman_sluka@partner.waters.com>
2025-11-02 20:27:31 +0100

    Added React query example.

--- 20. React Query/src/App.jsx
+++ 20. React Query/src/App.jsx
@@ -4,10 +4,12 @@ import {
   createBrowserRouter,
 } from 'react-router-dom';
 
+import { QueryClientProvider, } from '@tanstack/react-query';
 import Events from './components/Events/Events.jsx';
 import EventDetails from './components/Events/EventDetails.jsx';
 import NewEvent from './components/Events/NewEvent.jsx';
 import EditEvent from './components/Events/EditEvent.jsx';
+import { queryClient } from './utils/http.js';
 
 const router = createBrowserRouter([
   {
@@ -38,7 +40,11 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
--- 20. React Query/src/components/Events/EditEvent.jsx
+++ 20. React Query/src/components/Events/EditEvent.jsx
@@ -1,20 +1,73 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { fetchEvent, queryClient, updateEvent } from '../../utils/http.js';
+import LoadingIndicator from '../UI/LoadingIndicator.jsx';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function EditEvent() {
   const navigate = useNavigate();
+  const { id } = useParams();
 
-  function handleSubmit(formData) {}
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ['events', id],
+    queryFn: ({ signal }) => fetchEvent({ id, signal }),
+  });
+
+  const { mutate } = useMutation({
+    mutationFn: updateEvent,
+    onMutate: async ({ event }) => {
+      await queryClient.cancelQueries({ queryKey: ['events', id] });
+      const previousEvent = queryClient.getQueryData(['events', id]);
+      queryClient.setQueryData(['events', id], event);
+
+      return { previousEvent };
+    },
+    onError: (error, data, { previousEvent }) => {
+      queryClient.setQueryData(['events', id], previousEvent);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ['events', id] });
+    }
+  })
+
+  function handleSubmit(formData) {
+    mutate({ id, event: formData });
+    navigate('../');
+  }
 
   function handleClose() {
     navigate('../');
   }
 
-  return (
-    <Modal onClose={handleClose}>
-      <EventForm inputData={null} onSubmit={handleSubmit}>
+  let content;
+
+  if (isPending) {
+    content = (
+      <div className='center'>
+        <LoadingIndicator />
+      </div>
+    )
+  }
+
+  if (isError) {
+    content = (
+      <>
+        <ErrorBlock title="Failed to load event" message={error.info?.message || 'Please try again later.'} />
+        <div className="form-actions">
+          <Link to="../" className="button">
+            Okay
+          </Link>
+        </div>
+      </>
+    )
+  }
+
+  if (data) {
+    content = (
+      <EventForm inputData={data} onSubmit={handleSubmit}>
         <Link to="../" className="button-text">
           Cancel
         </Link>
@@ -22,6 +75,12 @@ export default function EditEvent() {
           Update
         </button>
       </EventForm>
+    )
+  }
+
+  return (
+    <Modal onClose={handleClose}>
+      {content}
     </Modal>
   );
 }
