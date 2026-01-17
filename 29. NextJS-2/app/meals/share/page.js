232e043529ab44bb150f5f990a436641c19159ad
Roman Sluka <roman_sluka@partner.waters.com>
2026-01-18 00:02:04 +0100

    Added new notes.

--- 29. NextJS-2/app/meals/share/page.js
+++ 29. NextJS-2/app/meals/share/page.js
@@ -1,7 +1,14 @@
+'use client'
+
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
+import { shareMeal } from '@/lib/actions';
+import MealsFormSubmit from '@/components/meals/meals-form-submit';
+import { useActionState } from 'react';
 
 export default function ShareMealPage() {
+    const [state, formAction] = useActionState(shareMeal, { message: null });
+
     return (
         <>
             <header className={classes.header}>
@@ -11,7 +18,7 @@ export default function ShareMealPage() {
                 <p>Or any other meal you feel needs sharing!</p>
             </header>
             <main className={classes.main}>
-                <form className={classes.form}>
+                <form className={classes.form} action={formAction}>
                     <div className={classes.row}>
                         <p>
                             <label htmlFor="name">Your name</label>
@@ -19,7 +26,7 @@ export default function ShareMealPage() {
                         </p>
                         <p>
                             <label htmlFor="email">Your email</label>
-                            <input type="email" id="email" name="email" required />
+                            <input type="email" id="email" name="email" />
                         </p>
                     </div>
                     <p>
@@ -39,9 +46,12 @@ export default function ShareMealPage() {
                             required
                         ></textarea>
                     </p>
-                    <ImagePicker />
+                    <ImagePicker label="Your image" name="image" />
+
+                    {state.message && <p>{state.message}</p>}
+
                     <p className={classes.actions}>
-                        <button type="submit">Share Meal</button>
+                        <MealsFormSubmit />
                     </p>
                 </form>
             </main>
--- 29. NextJS-2/lib/meals.js
+++ 29. NextJS-2/lib/meals.js
@@ -1,4 +1,8 @@
-const sql = require('better-sqlite3');
+import fs from 'node:fs';
+import sql from 'better-sqlite3';
+import slugify from 'slugify';
+import xss from 'xss';
+
 const db = sql('meals.db');
 
 export async function getMeals() {
@@ -15,4 +19,42 @@ export async function getMeals() {
 
 export async function getMeal(id) {
     return db.prepare('SELECT * FROM meals WHERE slug = ?').get(id);
+}
+
+export async function saveMeal(meal) {
+    await new Promise((res) => {
+        setInterval(() => {
+            res()
+        }, 2000);
+    })
+
+    meal.slug = slugify(meal.title, { lower: true });
+    meal.instructions = xss(meal.instructions);
+
+    const extension = meal.image.name.split('.').pop();
+    const fileName = `${meal.slug}.${extension}`
+
+    const stream = fs.createWriteStream(`public/images/${fileName}`);
+    const bufferedImage = await meal.image.arrayBuffer();
+
+    stream.write(Buffer.from(bufferedImage), (error) => {
+        if (error) {
+            throw new Error("Saving image failed!");
+        }
+    });
+
+    meal.image = `/images/${fileName}`;
+
+    return db.prepare(`
+      INSERT INTO meals VALUES (
+         null,
+         @slug,
+         @title,
+         @image,
+         @summary,
+         @instructions,
+         @creator,
+         @creator_email
+      )
+   `).run(meal);
 }
